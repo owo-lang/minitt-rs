f4605ea1d38bfa9b6d60c21ba19ee19084689a23
ice1000 <ice1000kotlin@foxmail.com>
2019-02-24 23:55:12 -0500

    [ eval ] Implement most parts `eval`

--- src/reduce.rs
+++ src/reduce.rs
@@ -1,10 +1,4 @@
-use crate::syntax::Closure;
-use crate::syntax::Expression;
-use crate::syntax::NameTrait;
-use crate::syntax::Neutral;
-use crate::syntax::Telescope;
-use crate::syntax::Telescope::UpDec;
-use crate::syntax::Value;
+use crate::syntax::*;
 use std::fmt::Debug;
 
 pub trait NameTraitAndDebug: NameTrait + Debug {}
@@ -73,6 +67,44 @@ impl<Name: NameTraitAndDebug> Expression<Name> {
     /// Evaluate an [`Expression`] to a [`Value`] under a [`Telescope`].
     /// Will panic if not well-typed.
     pub fn eval(self, context: &Telescope<Name>) -> Value<Name> {
-        unimplemented!()
+        match self {
+            Expression::Unit => Value::Unit,
+            Expression::One => Value::One,
+            Expression::Type => Value::Type,
+            Expression::Var(_) => unimplemented!(),
+            Expression::Sum(constructors) => {
+                Value::Sum((Box::new(constructors), Box::new(context.clone())))
+            }
+            Expression::Function(case_tree) => {
+                Value::Function((Box::new(case_tree), Box::new(context.clone())))
+            }
+            Expression::Pi(pattern, first, second) => Value::Pi(
+                Box::new(first.eval(context)),
+                Closure::Choice(pattern, *second, Box::new(context.clone())),
+            ),
+            Expression::Sigma(pattern, first, second) => Value::Sigma(
+                Box::new(first.eval(context)),
+                Closure::Choice(pattern, *second, Box::new(context.clone())),
+            ),
+            Expression::Lambda(pattern, body) => {
+                Value::Lambda(Closure::Choice(pattern, *body, Box::new(context.clone())))
+            }
+            Expression::First(pair) => pair.eval(context).value_first(),
+            Expression::Second(pair) => pair.eval(context).value_second(),
+            Expression::Application(function, argument) => {
+                function.eval(context).apply(argument.eval(context))
+            }
+            Expression::Pair(first, second) => Value::Pair(
+                Box::new(first.eval(context)),
+                Box::new(second.eval(context)),
+            ),
+            Expression::Constructor(name, body) => {
+                Value::Constructor(name, Box::new(body.eval(context)))
+            }
+            Expression::Declaration(declaration, rest) => {
+                rest.eval(&Telescope::UpDec(Box::new(context.clone()), *declaration))
+            }
+            e => panic!(format!("Cannot eval: {:?}", e)),
+        }
     }
 }
