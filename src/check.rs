b4d759a8796c6a24c43e2736cb6e40718b9d2a51
ice1000 <ice1000kotlin@foxmail.com>
2019-02-25 01:41:01 -0500

    [ check ] Initial type_checking. Add `upG`.

---
+++ src/check.rs
@@ -0,0 +1,47 @@
+use std::collections::BTreeMap;
+
+use crate::syntax::*;
+use crate::reduce::*;
+
+/// `Gamma` in Mini-TT.
+/// By doing this we get `lookupG` in Mini-TT for free.
+type Gamma<Name> = BTreeMap<Name, Value<Name>>;
+
+/// `genV` in Mini-TT.
+pub fn generate_value<Name: NameTrait>(id: u32) -> Value<Name> {
+    Value::Neutral(Neutral::Generated(id))
+}
+
+/// `upG` in Mini-TT.
+/// Gamma |- p : t = u => Gamma’
+///
+/// However, since Rust is an imperative language, we use mutable reference instead of making it
+/// monadic.
+pub fn update_gamma<'a, Name: DebuggableNameTrait>(
+    gamma: &'a mut Gamma<Name>,
+    pattern: &Pattern<Name>,
+    type_val: Value<Name>,
+    val: Value<Name>,
+) -> Result<&'a mut Gamma<Name>, String> {
+    match pattern {
+        Pattern::Pair(pattern_first, pattern_second) => match type_val {
+            Value::Sigma(first, second) => {
+                let val_first = val.clone().first();
+                update_gamma(gamma, pattern_first, *first, val_first.clone())
+                    // Gamma is updated now.
+                    .and_then(|gamma| update_gamma(
+                        gamma,
+                        pattern_second,
+                        second.instantiate(val_first),
+                        val.second(),
+                    ))
+            }
+            _ => Err(format!("Cannot update Gamma by: {:?}", pattern)),
+        },
+        Pattern::Var(name) => {
+            gamma.insert(name.clone(), type_val);
+            Ok(gamma)
+        }
+        Pattern::Unit => Ok(gamma),
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 /// Reduction: eval and eval's friends.<br/>
 /// Functions are basically put into `impl` blocks, their docs are not inside this module.
 pub mod reduce;
-/// Syntax: Term, Expression, Context.
+/// Syntax: term, expression, context.
+/// Methods are inside the `reduce` module.
 pub mod syntax;
+
+/// Normal form: when we read back, we get a normal form expression.
+pub mod normal;
+
+/// Type check: all about type checking.
+pub mod check;
