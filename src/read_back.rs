3ebecc32bffaf242d57f0bb6a6a2af4ee557f088
ice1000 <ice1000kotlin@foxmail.com>
2019-02-25 03:21:22 -0500

    [ comment ] Add some comments

--- src/read_back.rs
+++ src/read_back.rs
@@ -4,12 +4,15 @@ use crate::syntax::*;
 
 trait ReadBack {
     type NormalForm;
+
+    /// Interface for `rbV`, `rbN` and `rbRho` in Mini-TT.
     fn read_back(self) -> Self::NormalForm;
 }
 
 impl<Name: DebuggableNameTrait> ReadBack for Value<Name> {
     type NormalForm = NormalExpression<Name>;
 
+    /// `rbV` in Mini-TT.
     fn read_back(self) -> Self::NormalForm {
         unimplemented!()
     }
@@ -18,12 +21,15 @@ impl<Name: DebuggableNameTrait> ReadBack for Value<Name> {
 impl<Name: DebuggableNameTrait> ReadBack for Telescope<Name> {
     type NormalForm = NormalTelescope<Name>;
 
+    /// `rbRho` in Mini-TT.
     fn read_back(self) -> Self::NormalForm {
         use crate::syntax::GenericTelescope::*;
         match self {
             Nil => Nil,
             UpDec(context, declaration) => UpDec(Box::new(context.read_back()), declaration),
-            UpVar(context, pattern, val) => UpVar(Box::new(context.read_back()), pattern, val.read_back()),
+            UpVar(context, pattern, val) => {
+                UpVar(Box::new(context.read_back()), pattern, val.read_back())
+            }
         }
     }
 }
@@ -31,6 +37,7 @@ impl<Name: DebuggableNameTrait> ReadBack for Telescope<Name> {
 impl<Name: DebuggableNameTrait> ReadBack for Neutral<Name> {
     type NormalForm = NormalNeutral<Name>;
 
+    /// `rbN` in Mini-TT.
     fn read_back(self) -> Self::NormalForm {
         use crate::syntax::GenericNeutral::*;
         match self {
